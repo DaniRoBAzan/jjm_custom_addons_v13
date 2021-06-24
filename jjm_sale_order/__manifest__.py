d7596ab9c84db378afd16b40686423587cdde314
romina <danielabazan94@gmail.com>
2021-06-24 16:21:08 -0300

    [ADD]- first commit.

---
+++ jjm_sale_order/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'JJM Sale Order',
+    'description': 'this module makes improvements in the sales order.',
+    'author': "Romina Bazan",
+    'website':  '',
+    'license': 'OEEL-1',
+    'version':  '1.0',
+    'depends': [
+        'base',
+        'sale',
+        ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    'application': False,
+}
+
---
+++ jjm_sale_order/models/sale_order.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from odoo import _, api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+
+
+    def action_contract(self):
+        self.ensure_one()
+        action = self.env.ref('contract.contract_contract_customer_form_view').read()[0]
+        return action
+
+    def action_contract(self):
+        self.ensure_one()
+        action = self.env.ref('contract.contract_contract_customer_form_view').read()[0]
+        ctx = dict(
+            default_res_id=self.id,
+            default_partner_id=self.partner_id.id,
+        )
+        return {
+            'name': _('Contract'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'contract.contract',
+            'target': 'new',
+            'action': action,
+            'context': ctx,
+        }
+
+
