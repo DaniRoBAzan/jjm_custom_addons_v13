e50c39dc404e760a4c582f1ea6865d78787c448c
DaniRoBazan <danielabazan94@gmail.com>
2021-07-11 13:11:19 -0300

    [ADD] - first commit

--- jjm_contract/models/partner.py
+++ jjm_contract/models/partner.py
@@ -5,12 +5,17 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
 
-    paid_state = fields.Char(string='Estado de Deuda', required=True, compute='_compute_paid_state', readonly=True )
+    paid_state = fields.Char(string='Estado de Deuda', required=True, compute='_compute_paid_state', readonly=True ,default='no declarado')
+    paid_state_search = fields.Char(string='Estado Deuda Search', default='no declarado')
 
     def _compute_paid_state(self):
         for rec in self:
-            if rec.debt_balance <= 0:
-                rec.paid_state = 'Al día'
-            else:
-                rec.paid_state = 'Adeuda'
+            if rec.customer:
+                if rec.debt_balance <= 0:
+                    rec.paid_state = 'Al día'
+                    rec.paid_state_search = rec.paid_state
+                else:
+                    rec.paid_state = 'Adeuda'
+                    rec.paid_state_search = rec.paid_state
+
 
