76e832a571aa637a27c40fb72921048a791c5193
DaniRoBazan <danielabazan94@gmail.com>
2021-07-11 13:12:19 -0300

    [ADD] - first commit

--- jjm_products/__manifest__.py
+++ jjm_products/__manifest__.py
@@ -10,13 +10,9 @@
         'base',
         'product',
         'jjm_vehicles',
-        # 'partner_manual_rank',
-        # 'partner_contact_personal_information_page',
-        # 'partner_contact_birthdate'
         ],
     'data': [
         'views/product_view.xml',
-        # 'views/res_partner_purchase.xml',
     ],
     'application': False,
 }
