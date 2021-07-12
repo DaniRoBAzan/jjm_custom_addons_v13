21c368f76bf39e0d57ec16e2b76a1c6f73395deb
DaniRoBazan <danielabazan94@gmail.com>
2021-07-12 00:14:06 -0300

    [UPD] - actualizo descripcion de modulo.

--- jjm_contract/__manifest__.py
+++ jjm_contract/__manifest__.py
@@ -2,7 +2,11 @@
 
 {
     'name': 'JJM Contract',
-    'description': 'Add fields to the contract',
+    'description': '''
+    Agregue campo Estado deuda en el formulario de cliente, el mismo agrega automáticamente un estado "Al dia" si el cliente tiene seteado el campo Debt Balance en 0 o negativo ; de lo contrario, lo declara como un estado "Adeuda" .
+    Ademas agrego agrupación por "Estado de deuda" y filtrado por "Al día" - "Adeuda".
+    En el contrato agrega el campo Fecha Adhesión y el nombre del contrato se convierte en secuencial de 6 digitos arrancando con las siglas JJM.
+''',
     'author': "Romina Bazan",
     'license': 'OEEL-1',
     'version':  '1.0',
