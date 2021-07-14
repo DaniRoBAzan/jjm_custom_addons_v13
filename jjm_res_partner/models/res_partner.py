d77701a35b3367a290f919f6c4c7fd917ff2b521
DaniRoBazan <danielabazan94@gmail.com>
2021-07-14 15:12:16 -0300

    [UPD] - actualizo campo

--- jjm_res_partner/models/res_partner.py
+++ jjm_res_partner/models/res_partner.py
@@ -28,10 +28,10 @@ class ResPartner(models.Model):
     enterprise_state = fields.Char(string='Provincia', help='Escribe aqui el nombre de la Provincia de la Empresa del cliente.')
     enterprise_country = fields.Char(string='Pais', help='Escribe aqui el nombre del pais de la Empresa del cliente.')
 
-    is_consultant = fields.Boolean(string= 'Es Asesor', store=True, domain={'invisible': [('is_supplier','=',False)]})
+    is_consultant = fields.Boolean(string='Es Asesor / Vendedor', store=True, domain={'invisible': [('is_supplier','=',False)]})
     code_consultant = fields.Char(Strin='Codigo Asesor', store=True)
-    is_associated = fields.Boolean(string= 'Es Socio', store=True)
-    is_collector = fields.Boolean(string= 'Es Cobrador', store=True)
+    is_associated = fields.Boolean(string='Es Socio', store=True)
+    is_collector = fields.Boolean(string='Es Cobrador', store=True)
 
     #CONTRACT
     contract_ids = fields.One2many(
