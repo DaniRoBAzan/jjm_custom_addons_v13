5df1f653372b520ff3bbb12782ef0554a3bd8720
DaniRoBazan <danielabazan94@gmail.com>
2021-08-02 17:44:20 -0300

    [ADD] - Agrego reporte troquel

---
+++ jjm_report_troquel/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'JJM Report - Troquel',
+    'description': '''
+    Este modulo es el reporte del troquel.
+    Debe seleccionar una factura asociada al cliente y cliquear en el Accion/Imprimir Troquel.
+    ''',
+    'author': "Romina Bazan",
+    'license': 'OEEL-1',
+    'version':  '1.0',
+    'depends': [
+        'base',
+        'contacts',
+        'sale',
+        'product',
+        ],
+    'data': [
+        'report/troquel_view.xml',
+    ],
+    'application': False,
+}
---
+++ jjm_report_troquel/report/troquel.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
