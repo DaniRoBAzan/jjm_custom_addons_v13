f2971bb6b8826ffedb39497493d38e79936c579f
DaniRoBazan <danielabazan94@gmail.com>
2021-09-05 13:58:46 -0300

    [UPD] - Actualizo partner

--- jjm_res_partner/__manifest__.py
+++ jjm_res_partner/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-
 {
     'name': 'JJM CONTACTOS',
     'description': '''
     - Agrego solapa de informacion laboral con los datos de la empresa
-    - Agrego solapa de informacion personal 
+    - Agrego solapa de informacion personal
     - Agrego el campo Documentos a la vista tree
     - cambio nombre de campos de origen por Ocupacion y Barrio.
     - Agrego campos en solapa Compra y Ventas: Es Socio, Es Supervisor, Es Asesor / Vendedor,
     Codigo Asesor, Es Cobrador  y Encargado.
-    - En Contrato/Partner/Facturacion y Contabilidad, se te habilita la opción "Tiene tarjeta", 
+    - En Contrato/Partner/Facturacion y Contabilidad, se te habilita la opción "Tiene tarjeta",
     al tildar aparece la opción de cargar los datos del banco y numero de tarjeta.
     ''',
     'author': "Romina Bazan",
