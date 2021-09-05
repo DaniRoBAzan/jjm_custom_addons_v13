8965b604a45164178a0825b845b4b208010e7b28
DaniRoBazan <danielabazan94@gmail.com>
2021-09-05 17:27:11 -0300

    [UPD] - Actualizo reporte

--- jjm_change_plan/models/contract.py
+++ jjm_change_plan/models/contract.py
@@ -8,6 +8,19 @@ class ContractContract(models.Model):
 
     parent_contract = fields.Integer('Contrato Padre')
 
+    def get_jjm_last_canon_contract(self):
+        #con el name del contrato actual debo buscar las facturas pagadas
+        invoice_obj = self.env['account.move'].search([('invoice_origin', '=', self.name),('amount_residual','=',0)])
+        if invoice_obj:
+            #contarlas y setear a ultimo canon.
+            self.jjm_last_canon_contract= len(invoice_obj)
+            return len(invoice_obj)
+        else:
+            return False
+
+    def get_payment_term(self):
+        return 9
+
     def action_create_contract(self):
         action = self.env.ref('contract.contract_contract_customer_form_view').read()[0]
         self.action_cancel()  # cancelamos el contrato actual.
@@ -19,8 +32,9 @@ class ContractContract(models.Model):
             default_consultant_id=self.consultant_id.id,
             default_collector_id=self.collector_id.id,
             default_method_payment_id=self.method_payment_id.id,
+            #default_payment_term_id=self.get_payment_term(),
             default_jjm_description_moto=self.jjm_description_moto,
-            default_jjm_last_canon_contract=self.jjm_last_canon_contract,
+            default_jjm_last_canon_contract=self.get_jjm_last_canon_contract(),
         )
         return {
             'name': _('Contrato'),
